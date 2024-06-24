2a0cf8f22ad3f99cac782e5e3eb6eb4a0cde32c1
AyaZayed <ayazayed@std.mans.edu.eg>
2024-06-25 00:04:24 +0300

    prisma import error

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -1,5 +1,6 @@
 import HistoryChart from "@/components/HistoryChart";
 import { getUserByClerkId } from "@/utils/auth";
+import { prisma } from "@/utils/db";
 
 const getData = async () => {
   const user = await getUserByClerkId();
